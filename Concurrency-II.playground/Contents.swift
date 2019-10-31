a863daa4d59eaab8e1d06291471de59b71573e0f
AustinPotts <pottsevolvements@gmail.com>
2019-10-31 17:58:42 -0400

    Configured Real World Ex. Using Locks
    With Network Requests

--- Concurrency-II.playground/Contents.swift
+++ Concurrency-II.playground/Contents.swift
@@ -1,7 +1,8 @@
 import Foundation
 
 var x = 42
-let lockForX = NSLock()
+
+let sharedAccessQueue = DispatchQueue(label: "Shared Resource Queue")
 
 extension NSLock {
     func withLock(_ work: () -> Void){ //This will help lock & unlock NSLock without having to call both
@@ -12,18 +13,52 @@ extension NSLock {
 }
 
 //This will not print out 142 because there is no thread safety, each computer runs the same thing at the same time without
-DispatchQueue.concurrentPerform(iterations: 100) { _ in
-  
-    lockForX.withLock {
-         var localCopy = x
-           localCopy += 1
-           x = localCopy
+DispatchQueue.concurrentPerform(iterations: 100) { threadNumber in
+    
+    sharedAccessQueue.sync {
+        
+        print("Executing concurrent perform \(threadNumber + 1)")
+        var localCopy = x
+        localCopy += 1
+        x = localCopy
     }
-   
+  
+//    lockForX.withLock {
+//         var localCopy = x
+//           localCopy += 1
+//           x = localCopy
+//    }
     //Using locks allows us to effectiley access
-   
 }
-
 print(x)
 
 
+
+var listOfNames: [String] = []
+let nameLock = NSLock()
+
+//Lock resources that will be concurrently accessed
+
+URLSession.shared.dataTask(with: URL(string: "https://swapi.co/people/1/")!) { (data, response, error) in
+    nameLock.lock()
+    listOfNames.append("Luke")
+    print(listOfNames)
+    nameLock.unlock()
+       
+}.resume()
+
+URLSession.shared.dataTask(with: URL(string: "https://swapi.co/people/1/")!) { (data, response, error) in
+          nameLock.lock()
+          listOfNames.append("Han")
+          print(listOfNames)
+          nameLock.unlock()
+       
+}.resume()
+
+URLSession.shared.dataTask(with: URL(string: "https://swapi.co/people/1/")!) { (data, response, error) in
+          nameLock.lock()
+          listOfNames.append("Yoda")
+          print(listOfNames)
+          nameLock.unlock()
+       
+}.resume()
