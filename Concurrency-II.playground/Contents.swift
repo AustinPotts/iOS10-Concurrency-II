bace8a840992b4bc7819d422dc8e23f9fa2d5084
AustinPotts <pottsevolvements@gmail.com>
2019-10-31 16:27:20 -0400

    Configured NSLock Extension

--- Concurrency-II.playground/Contents.swift
+++ Concurrency-II.playground/Contents.swift
@@ -3,14 +3,25 @@ import Foundation
 var x = 42
 let lockForX = NSLock()
 
+extension NSLock {
+    func withLock(_ work: () -> Void){ //This will help lock & unlock NSLock without having to call both
+        lock()
+        work()
+        unlock()
+    }
+}
+
 //This will not print out 142 because there is no thread safety, each computer runs the same thing at the same time without
 DispatchQueue.concurrentPerform(iterations: 100) { _ in
-    lockForX.lock()
-    var localCopy = x
-    localCopy += 1
-    x = localCopy
-    //Using locks allows us to effectiley access 
-    lockForX.unlock()
+  
+    lockForX.withLock {
+         var localCopy = x
+           localCopy += 1
+           x = localCopy
+    }
+   
+    //Using locks allows us to effectiley access
+   
 }
 
 print(x)
