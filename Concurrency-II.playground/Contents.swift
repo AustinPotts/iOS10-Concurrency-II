22145c59996311280a382e846e709e29932ccb8a
AustinPotts <pottsevolvements@gmail.com>
2019-10-31 16:16:59 -0400

    Configured Lock
    Added NSLock to prevent multiple threads from accessing shared resources at the same time

--- Concurrency-II.playground/Contents.swift
+++ Concurrency-II.playground/Contents.swift
@@ -1,3 +1,18 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+var x = 42
+let lockForX = NSLock()
+
+//This will not print out 142 because there is no thread safety, each computer runs the same thing at the same time without
+DispatchQueue.concurrentPerform(iterations: 100) { _ in
+    lockForX.lock()
+    var localCopy = x
+    localCopy += 1
+    x = localCopy
+    //Using locks allows us to effectiley access 
+    lockForX.unlock()
+}
+
+print(x)
+
+
